8774d9eaeeb4e91332a64ce8094374a482128dc9
kakahikari <kakahikari@gmail.com>
2017-07-24 11:58:58 +0800

    開發站改回設定檔site-domain

--- setConfig.js
+++ setConfig.js
@@ -1,11 +1,13 @@
 let _API_URL = process.argv[2]
+let _SITE_DOMAIN = process.argv[3]
 
 let today = new Date()
 let _BUILD_DATE = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()} ${today.getHours()}:${today.getMinutes()}`
 
 let content =
 `export const BUILD_DATE = '${_BUILD_DATE}'\n` +
-`export const API_URL = '${_API_URL}'\n`
+`export const API_URL = '${_API_URL}'\n` +
+`export const SITE_DOMAIN = '${_SITE_DOMAIN}'\n`
 
 var fs = require('fs')
 
@@ -16,8 +18,8 @@ let setConfig = () => {
       ******************************
         > BUILD_DATE: ${_BUILD_DATE}
         > API_URL: ${_API_URL}
-      ******************************
-    `)
+        > SITE_DOMAIN: ${_SITE_DOMAIN}
+      ******************************`)
   })
 }
 
