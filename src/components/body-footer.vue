6ab7a19796eb10cc1d9c504a3acb651fc6891f5a
kakahikari <kakahikari@gmail.com>
2017-07-28 18:13:27 +0800

    footer加入版本顯示

--- src/components/body-footer.vue
+++ src/components/body-footer.vue
@@ -1,17 +1,21 @@
 <template lang="pug">
-  footer {{ sitename[language] }} ALL RIGHTS RESERVED.
+  footer(@click="toogleVersion()") {{ sitename[language] }} ALL RIGHTS RESERVED.
+    span.version(v-if="showVersion") v{{ version }}
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import { SITENAME } from '@/siteConfig'
+import { _VERSION } from 'src/version'
 
 export default {
   name: 'body-footer',
 
   data () {
     return {
-      sitename: SITENAME
+      sitename: SITENAME,
+      version: _VERSION,
+      showVersion: false
     }
   },
 
@@ -19,14 +23,24 @@ export default {
     language: state => {
       return state.USER.language
     }
-  })
+  }),
+
+  methods: {
+    toogleVersion () {
+      this.showVersion = !this.showVersion
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
   footer {
+    position: relative;
     padding: .5em 0;
     font-size: .75em;
     text-align: center;
+  }
+  .version {
+    margin-left: 1em;
   }
 </style>
