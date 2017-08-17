6ed2f5f03bcf6e74feb14f0fae41957abb00436b
kakahikari <kakahikari@gmail.com>
2017-08-17 16:43:43 +0800

    修正微信機制, 改為顯示qrcode

--- src/pages/account/deposit/weixin.vue
+++ src/pages/account/deposit/weixin.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
   section.deposit
-    .form__fields
-      mt-field(":label"="$root.i18n('Amount')" v-model="formData.amount")
-      form-errors(":errors"="$v.formData.amount")
-        form-error(v-if="!$v.formData.amount.between") {{ $root.i18n('must between') }} 50 ~ 1,000,000
-    .form__actions
-      mt-button.form__actions__btn.submit(@click="action(formData)") {{ $root.i18n('Submit') }}
-      mt-button.form__actions__btn(@click="init()") {{ $root.i18n('Reset') }}
+    div(v-if="qrcode")
+      .form__actions.center
+        p 扫描下图存款
+        div("v-html"="qrcode")
+    div(v-else)
+      .form__fields
+        mt-field(":label"="$root.i18n('Amount')" v-model="formData.amount")
+        form-errors(":errors"="$v.formData.amount")
+          form-error(v-if="!$v.formData.amount.between") {{ $root.i18n('must between') }} 50 ~ 1,000,000
+      .form__actions
+        mt-button.form__actions__btn.submit(@click="action(formData)") {{ $root.i18n('Submit') }}
+        mt-button.form__actions__btn(@click="init()") {{ $root.i18n('Reset') }}
     vAutoSendForm(":params"="actions" v-if="!!actions.url")
 </template>
 
@@ -17,6 +22,7 @@
   import formError from '@/components/form-errors/form-error'
   import vAutoSendForm from '@/components/form/v-auto-send-form'
   import { amount } from '@/validators/config'
+  import qrcode from 'qrcode-npm'
   import { Field, Button, DatetimePicker, Indicator } from 'mint-ui'
   Vue.component(Field.name, Field)
   Vue.component(Button.name, Button)
@@ -34,7 +40,8 @@
         formData: {
           amount: '',
           type: 'weixin_scan'
-        }
+        },
+        qrcode: ''
       }
     },
 
@@ -59,9 +66,10 @@
         Indicator.open()
         DepositService.DinPay({context: this, body: formData}).then((res) => {
           Indicator.close()
-          this.$root.showToast({content: this.$root.i18n('success')})
-          this.actions.keys = res.params
-          this.actions.url = res.url
+          var qr = qrcode.qrcode(4, 'M')
+          qr.addData(res.url)
+          qr.make()
+          this.qrcode = qr.createImgTag(4)
           this.init()
         }).catch((err) => {
           Indicator.close()
