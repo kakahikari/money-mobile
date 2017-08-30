5608e6d0813271d9c4f4e5bf3de3ab810d34f3bc
kakahikari <kakahikari@gmail.com>
2017-08-30 14:29:28 +0800

    deposit增加新的設定檔開關(#7025)

--- src/pages/account/deposit/index.vue
+++ src/pages/account/deposit/index.vue
@@ -1,34 +1,33 @@
 <template lang="pug">
-  section.deposit
-    mt-cell(":title"="$root.i18n('Bank deposit')" is-link ":to"="{name: 'Bank-deposit'}")
-    mt-cell(":title"="$root.i18n('Online deposit')" is-link ":to"="{name: 'Online-deposit'}")
-    mt-cell(":title"="$root.i18n('Weixin deposit')" is-link ":to"="{name: 'Weixin-deposit'}")
+section.deposit
+  mt-cell(":title"="$root.i18n('Bank deposit')" is-link ":to"="{name: 'Bank-deposit'}" v-if="checkFunctionEnable('bank')")
+  mt-cell(":title"="$root.i18n('Online deposit')" is-link ":to"="{name: 'Online-deposit'}" v-if="checkFunctionEnable('online')")
+  mt-cell(":title"="$root.i18n('Weixin deposit')" is-link ":to"="{name: 'Weixin-deposit'}" v-if="checkFunctionEnable('weixin')")
 </template>
 
 <script>
-  import Vue from 'vue'
-  import { Cell } from 'mint-ui'
-  Vue.component(Cell.name, Cell)
+import Vue from 'vue'
+import { Cell } from 'mint-ui'
+import { SITE_FUNCTIONS } from '@/siteConfig'
+Vue.component(Cell.name, Cell)
 
-  export default {
-    name: 'deposit',
+export default {
+  name: 'deposit',
 
-    data () {
-      return {
-        bank: {
-          bankName: '',
-          playername: '',
-          account: ''
-        },
-        formData: {
-          amount: '',
-          dateTime: '',
-          bankName: '',
-          account: '',
-          playername: '',
-          orderNo: ''
-        }
-      }
+  data () {
+    return {
+      siteFunctions: SITE_FUNCTIONS
+    }
+  },
+
+  methods: {
+    checkFunctionEnable (functionName) {
+      let target = this.siteFunctions.filter(node => node.name === 'deposit')
+      if (!target.length > 0) return false
+      let targetFunction = target[0].functions.filter(node => node.name === functionName)
+      if (targetFunction.length > 0) return targetFunction[0].enable
+      return false
     }
   }
+}
 </script>
