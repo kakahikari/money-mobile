5309f2adaea7d89d651be6422b0bb4722eb8ae0d
kakahikari <kakahikari@gmail.com>
2017-09-06 18:30:09 +0800

    deposit/bank功能異動(#7026) - 增加匯款提示 - 增加IDR(K)幣別提示 - 設定檔加入orderNo, playernameEditable開關

--- src/pages/account/deposit/bank.vue
+++ src/pages/account/deposit/bank.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
   section.deposit
+    .form__fields.no-border
+      div {{ $root.i18n('Please contact customer service for deposit bank account') }}
     .form__fields
       div(@click="openPicker()")
         mt-field(":label"="$root.i18n('Deposit date')" ":disabled"="true" v-model="formData.dateTime")
@@ -15,12 +17,14 @@
               template(v-for="node in bankList")
                 option(":value"="node") {{ node.bankName }} - {{ node.account }}
       form-errors(":errors"="$v.bank")
-      mt-field(":label"="$root.i18n('Bank ACC name')" ":readonly"="true" v-model="formData.playername")
-      mt-field(":label"="$root.i18n('Order No')" v-model="formData.orderNo")
-      form-errors(":errors"="$v.formData.orderNo")
+      mt-field(":label"="$root.i18n('Bank ACC name')" ":readonly"="!checkFunctionEnable('playernameEditable')" v-model="formData.playername")
+      template(v-if="checkFunctionEnable('orderNo')")
+        mt-field(":label"="$root.i18n('Order No')" v-model="formData.orderNo")
+        form-errors(":errors"="$v.formData.orderNo")
       mt-field(":label"="$root.i18n('Amount')" v-model="formData.amount")
       form-errors(":errors"="$v.formData.amount")
     .form__fields.no-border
+      div(v-if="currency === 'IDR(K)'") {{ $root.i18n('The IDR amount will be exchanged into IDR(K) in your purse at the rate of 1000：1') }}
       .form__actions__link
         router-link(":to"="{name: 'Bankcard'}") {{ $root.i18n('No linking bank card?') }}
         icon(name="keyboard_arrow_right")
@@ -35,6 +39,7 @@
   import Vue from 'vue'
   import { mapState } from 'vuex'
   import formErrors from '@/components/form-errors'
+  import { CURRENCY, SITE_FUNCTIONS } from '@/siteConfig'
   import { bank, date, depositAmount, orderNo } from '@/validators/config'
   import { Field, Button, DatetimePicker, Indicator } from 'mint-ui'
   Vue.component(Field.name, Field)
@@ -58,7 +63,9 @@
           account: '',
           playername: '',
           orderNo: ''
-        }
+        },
+        currency: CURRENCY,
+        siteFunctions: SITE_FUNCTIONS
       }
     },
 
@@ -127,6 +134,15 @@
       },
       dateTimeChange (val) {
         this.formData.dateTime = moment(val).format('YYYY-MM-DD')
+      },
+      checkFunctionEnable (functionName) {
+        let target = this.siteFunctions.filter(node => node.name === 'deposit')
+        if (!target.length > 0) return false
+        target = target[0].functions.filter(node => node.name === 'bank')
+        if (!target.length > 0) return false
+        let targetFunction = target[0].functions.filter(node => node.name === functionName)
+        if (targetFunction.length > 0) return targetFunction[0].enable
+        return false
       }
     },
 
